5782401f2c1a035521166f2f8a9e505abd046870
Lars Peter Thomsen <larsp@magenta-aps.dk>
2024-04-17 13:12:29 +0200

    feat: [#60275] django docker setup

---
+++ docker/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+MAKE_MIGRATIONS=${MAKE_MIGRATIONS:=false}
+MIGRATE=${MIGRATE:=false}
+TEST=${TEST:=false}
+MAKEMESSAGES=${MAKEMESSAGES:=false}
+COMPILEMESSAGES=${COMPILEMESSAGES:=false}
+DJANGO_DEBUG=${DJANGO_DEBUG:=false}
+
+python manage.py wait_for_db
+
+if [ "${MAKE_MIGRATIONS,,}" = true ]; then
+  echo 'generating migrations'
+  python manage.py makemigrations --no-input
+fi
+if [ "${MIGRATE,,}" = true ]; then
+  echo 'running migrations'
+  python manage.py migrate
+fi
+
+echo 'collecting static files'
+python manage.py collectstatic --no-input --clear
+
+if [ "${TEST,,}" = true ]; then
+  echo 'running tests'
+  python manage.py test
+fi
+if [ "${MAKEMESSAGES,,}" = true ]; then
+  echo 'making messages'
+  python manage.py makemessages --locale=kl --no-obsolete --add-location file
+  python manage.py makemessages --locale=da --no-obsolete --add-location file
+fi
+if [ "${COMPILEMESSAGES,,}" = true ]; then
+  echo 'compiling messages'
+  python manage.py compilemessages --locale=kl
+  python manage.py compilemessages --locale=da
+fi
+
+exec "$@"
---
+++ havneafgifter/havneafgifter/apps.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from django.apps import AppConfig
+
+
+class HavneafgifterConfig(AppConfig):
+    name = "havneafgifter"
