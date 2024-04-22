d0bd445a6d51650ea38d9c3c6904a4703bec4342
Mads Riis-Weitling <mrw@magenta.dk>
2024-04-22 09:43:01 +0200

    chore: [#60305] Disable Django template cache when `DEBUG` == `True`

--- havneafgifter/project/settings.py
+++ havneafgifter/project/settings.py
@@ -61,11 +61,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "project.urls"
 
+default_loaders = [
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+]
+
+cached_loaders = [("django.template.loaders.cached.Loader", default_loaders)]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -73,6 +79,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "loaders": default_loaders if DEBUG else cached_loaders,
         },
     },
 ]
