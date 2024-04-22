f148f2e7ccce6b2c0080c50f65ab03a5727fa735
Mads Riis-Weitling <mrw@magenta.dk>
2024-04-22 18:07:07 +0200

    test: [#60305] Add tests for `PassengerTaxCreateView`

--- havneafgifter/havneafgifter/tests/test_views.py
+++ havneafgifter/havneafgifter/tests/test_views.py
@@ -1,9 +1,14 @@
+from unittest.mock import patch
+
+from django.forms import BaseFormSet
 from django.http import HttpResponseRedirect
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from unittest_parametrize import ParametrizedTestCase, parametrize
 
-from ..models import CruiseTaxForm, HarborDuesForm, ShipType
+from ..forms import PassengersByCountryForm
+from ..models import CruiseTaxForm, HarborDuesForm, Nationality, ShipType
+from ..views import PassengerTaxCreateView
 from .mixins import HarborDuesFormMixin
 
 
@@ -40,4 +45,46 @@ class TestHarborDuesFormCreateView(ParametrizedTestCase, HarborDuesFormMixin, Te
         self.assertEqual(
             response.url,
             reverse(next_view_name, kwargs={"pk": instance.pk}),
+        )
+
+
+class TestPassengerTaxCreateView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.request_factory = RequestFactory()
+        cls.get_request = cls.request_factory.get("")
+        cls.instance = PassengerTaxCreateView()
+
+    def test_setup(self):
+        with patch.object(CruiseTaxForm.objects, "get") as mock_get:
+            self.instance.setup(self.get_request, pk=0)
+            mock_get.assert_called_once_with(pk=0)
+
+    def test_get_form_returns_formset(self):
+        self.instance.request = self.get_request
+        formset = self.instance.get_form()
+        self.assertIsInstance(formset, BaseFormSet)
+        self.assertIs(formset.form, PassengersByCountryForm)
+        self.assertFalse(formset.can_order)
+        self.assertFalse(formset.can_delete)
+        self.assertEqual(formset.extra, 0)
+
+    def test_get_form_kwargs_populates_initial(self):
+        self.instance.request = self.get_request
+        form_kwargs = self.instance.get_form_kwargs()
+        self.assertListEqual(
+            form_kwargs["initial"],
+            [
+                {"nationality": nationality, "number_of_passengers": 0}
+                for nationality in Nationality
+            ],
+        )
+
+    def test_get_context_data_populates_formset(self):
+        self.instance.request = self.get_request
+        context_data = self.instance.get_context_data()
+        self.assertIsInstance(
+            context_data["passengers_by_country_formset"],
+            BaseFormSet,
         )
