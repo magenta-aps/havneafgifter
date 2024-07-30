9f1a0973674be1331cdee4667414ed38e4400e88
Mads Riis-Weitling <mrw@magenta.dk>
2024-07-30 12:58:09 +0200

    feat: [#61559] Add "password reset" flow (URLs, templates, whitelisting)
    The templates use the default English texts from the default Django
    "password reset" templates, so that we can rely on existing Danish
    translations. This means that we do not have to fill out the
    corresponding entries in the `da` locale.
    
    However, there are no Greenlandic translations in Django for these
    strings. Currently, users who select Greenlandic will see English texts
    for the password reset flow.

--- havneafgifter/project/urls.py
+++ havneafgifter/project/urls.py
@@ -2,6 +2,7 @@ from typing import List
 
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import URLPattern, URLResolver, include, path
 from django.views.i18n import JavaScriptCatalog
 from django_mitid_auth.saml.views import MetadataView
@@ -21,6 +22,34 @@ urlpatterns: List[URLResolver | URLPattern] = [
     path("saml/", include("django_mitid_auth.urls", namespace="mitid")),
     path("saml2/metadata/", MetadataView.as_view(), name="saml_metadata_override"),
     path("metrics/", include("metrics.urls")),
+    path(
+        "password/reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="havneafgifter/user/password/reset_begin.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password/reset/sent/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="havneafgifter/user/password/reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="havneafgifter/user/password/reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password/reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="havneafgifter/user/password/reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
 if settings.MITID_TEST_ENABLED:
     urlpatterns.append(
