6ff773198a12ed5dd93aea1b673d2e95a2b25d02
Tim Engel <te@magenta.dk>
2024-08-27 10:32:14 +0200

    fix: [#61609] Adjust eslint package.json and add jquery env to config, to silence false errors

--- .eslint/eslint.config.js
+++ .eslint/eslint.config.js
@@ -4,17 +4,23 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      },
-      ecmaVersion: 6,
-      sourceType: "script",
-    },
-    rules: {
-      "no-case-declarations": "off",
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            },
+            ecmaVersion: 6,
+            sourceType: "script",
+        },
+        env: {
+            browser: true,
+            commonjs: true,
+            es6: true,
+            jquery: true
+        },
+        rules: {
+            "no-case-declarations": "off",
+        }
     }
-  }
 ];
