3b5ef7da44b423eb4c0fb4ec5652cbb78824feb2
Tim Engel <te@magenta.dk>
2024-09-06 18:27:29 +0200

    fix: [61609] fix broken TaxRateForm form insert tests

--- havneafgifter/havneafgifter/views.py
+++ havneafgifter/havneafgifter/views.py
@@ -877,8 +877,6 @@ class TaxRateFormView(LoginRequiredMixin, UpdateView):
         return kwargs
 
     def get_port_formset(self):
-        ic()
-        ic(self.clone)
         if self.clone:
             initial = (
                 [
@@ -901,7 +899,6 @@ class TaxRateFormView(LoginRequiredMixin, UpdateView):
             formset.extra = len(initial)
             return formset
         else:
-            ic()
             return PortTaxRateFormSet(self.request.POST or None, instance=self.object)
 
     def get_disembarkmentrate_formset(self):
