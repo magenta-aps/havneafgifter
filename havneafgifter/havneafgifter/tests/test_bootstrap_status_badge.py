30539e2dbf875c6f54752c6b8ae610ade15df013
Mads Riis-Weitling <mrw@magenta-aps.dk>
2024-12-13 16:47:16 +0100

    feat: [#62742] Introduce `base_default.html` and `base_bare.html`

---
+++ havneafgifter/havneafgifter/tests/test_bootstrap_status_badge.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+
+from havneafgifter.models import HarborDuesForm, Status
+from havneafgifter.templatetags.bootstrap_status_badge import bootstrap_status_badge
+
+
+class TestBootstrapStatusBadge(SimpleTestCase):
+    def test_badget(self):
+        form = HarborDuesForm(status=Status.DRAFT)
+        context: dict = bootstrap_status_badge(form)
+        self.assertIs(context["form"], form)
+        self.assertEqual(context["bg"], "bg-secondary")
