232ab9e12fc1d804aadd7f5c3f0c74bb66474d38
Lars Peter Thomsen <larsp@magenta.dk>
2024-12-19 13:42:54 +0100

    feat: [#61839] Undgå network_mode: host

--- dev-environment/hosts.sh
+++ dev-environment/hosts.sh
@@ -3,7 +3,7 @@
 hosts_file="/hosts"
 
 add_hosts=""
-for hostname in havneafgifter-web havneafgifter-idp; do
+for hostname in havneafgifter-web havneafgifter-idp havneafgifter-mailhog; do
   if ! grep $hostname $hosts_file; then
     add_hosts+=" $hostname"
   fi
