d2f28d27960e995c4442f91be417a0878a87da45
Peter Juelsgaard <pj@magenta.dk>
2025-08-06 13:02:43 +0200

    refactor: [#65779] Remove useless statement

--- havneafgifter/havneafgifter/templatetags/bootstrap_modal.py
+++ havneafgifter/havneafgifter/templatetags/bootstrap_modal.py
@@ -37,7 +37,6 @@ def reject_form_modal(context, form: HarborDuesForm | CruiseTaxForm) -> dict:
 
 @register.inclusion_tag(
     "havneafgifter/bootstrap/delete_form.html",
-    takes_context=True,
 )
 def delete_form_modal(context, form: HarborDuesForm | CruiseTaxForm) -> dict:
     return {"form": form}
