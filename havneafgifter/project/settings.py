7670c0f9a3be7008e19e952058872c0686f343b5
Nick Janssen <nj@magenta-aps.dk>
2025-08-06 11:42:16 +0200

    fix: [#65779] Fix static_url and static_root
    This will stop collectstatic from looping

--- havneafgifter/project/settings.py
+++ havneafgifter/project/settings.py
@@ -213,8 +213,9 @@ FORMAT_MODULE_PATH = ["havneafgifter.formats"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "/static"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
