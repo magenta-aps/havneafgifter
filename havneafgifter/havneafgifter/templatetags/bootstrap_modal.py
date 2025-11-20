1866b616db5e5fc639d648239616b925f2b5e926
Lars Peter Thomsen <larsp@magenta-aps.dk>
2025-11-20 13:10:10 +0100

    feat: [#62586] remove approval, rejection and withdrawal

--- havneafgifter/havneafgifter/templatetags/bootstrap_modal.py
+++ havneafgifter/havneafgifter/templatetags/bootstrap_modal.py
@@ -1,38 +1,8 @@
 from django import template
 
-from havneafgifter.forms import ReasonForm
 from havneafgifter.models import CruiseTaxForm, HarborDuesForm, Vessel
 
 register = template.Library()
-
-
-@register.inclusion_tag(
-    "havneafgifter/bootstrap/withdraw_form.html",
-    takes_context=True,
-)
-def withdraw_form_modal(context, form: HarborDuesForm | CruiseTaxForm) -> dict:
-    return {"form": form}
-
-
-@register.inclusion_tag(
-    "havneafgifter/bootstrap/approve_form.html",
-    takes_context=True,
-)
-def approve_form_modal(context, form: HarborDuesForm | CruiseTaxForm) -> dict:
-    return {"form": form}
-
-
-@register.inclusion_tag(
-    "havneafgifter/bootstrap/reject_form.html",
-    takes_context=True,
-)
-def reject_form_modal(context, form: HarborDuesForm | CruiseTaxForm) -> dict:
-    return {
-        "form": form,
-        "reason_form": ReasonForm(),
-        # required to render `{{ request.csp_nonce }}`
-        "request": context.get("request"),
-    }
 
 
 @register.inclusion_tag(
